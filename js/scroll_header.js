e98b061d9c24ba07c211b68797d71dde8782553d
Bluee1509 <anhhienvo.1509@gmail.com>
2024-05-02 14:33:50 +0700

    2/5/2024

--- js/scroll_header.js
+++ js/scroll_header.js
@@ -1,8 +1,16 @@
 window.addEventListener("scroll", function () {
   var header = document.getElementById("myHeader");
+  var navLinks = document.querySelectorAll('.header__nav ul li a');
+
   if (window.scrollY > 0) {
     header.classList.add("scrolled");
+    navLinks.forEach(link => {
+      link.classList.add('scrolled')
+    });
   } else {
     header.classList.remove("scrolled");
+    navLinks.forEach(link => {
+      link.classList.remove('scrolled')
+    });
   }
 });
